0f4480446d94d7efee4d843e4bdee9192b8b07fa
Jason_Shu <shuyue524@gmail.com>
2019-02-16 22:03:50 +0800

    npm run build add homepage

---
+++ build/precache-manifest.eeebb2e60490e16b9d893c774f34764c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e55570d879d4975b3de7",
+    "url": "/TodoList-demo-1/build/static/js/runtime~main.e55570d8.js"
+  },
+  {
+    "revision": "d6e7dba8a4ef553e294d",
+    "url": "/TodoList-demo-1/build/static/js/main.d6e7dba8.chunk.js"
+  },
+  {
+    "revision": "8ed6f4afb181c1256a10",
+    "url": "/TodoList-demo-1/build/static/js/2.8ed6f4af.chunk.js"
+  },
+  {
+    "revision": "d6e7dba8a4ef553e294d",
+    "url": "/TodoList-demo-1/build/static/css/main.ccaec136.chunk.css"
+  },
+  {
+    "revision": "8ed6f4afb181c1256a10",
+    "url": "/TodoList-demo-1/build/static/css/2.52d94fb7.chunk.css"
+  },
+  {
+    "revision": "2339f126297e91ea98963f9deb1002a1",
+    "url": "/TodoList-demo-1/build/index.html"
+  }
+];
